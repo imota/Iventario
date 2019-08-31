99266e1ae783de94cb23982689b7ce2010f04265
guibytes <guilhermenatal47@gmail.com>
2019-08-31 17:44:29 -0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-
-const StyledButton = styled.button`
-  border: solid 1px #222;
-  background: ${props => props.color};
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-`;
-
-const StyledDiv = styled.div`
-  text-align: center;
-`;
-
-const StyledText = styled.p`
-  cursor: pointer;
-  border: solid 1px #222;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-  width: fit-content;
-  padding: 10px;
-  margin: 10px;
-  :hover {
-    background: #22f;
-  }
-  transition: all 1s;
-`;
-
-const ButtonProps = props => {
-  return (
-    <StyledText onClick={() => props.updateClick()}>
-      Clicked {props.times}
-    </StyledText>
-  );
-};
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Inicio from "./Inicio";
 
 class App extends Component {
   constructor(props) {
@@ -47,13 +18,12 @@ class App extends Component {
 
   render() {
     return (
-      <StyledDiv className="App">
-        <StyledButton color={this.state.color}>hello world</StyledButton>
-        <ButtonProps
-          updateClick={this.updateClick.bind(this)}
-          times={this.state.times}
-        />
-      </StyledDiv>
+      <div>
+        <Router>
+          <Route exact path="/" component={Inicio} />
+          <Route path="/inicio" component={Inicio} />
+        </Router>
+      </div>
     );
   }
 }
---
+++ src/Inicio/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  border: solid 1px #222;
+  background: ${props => props.color};
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+`;
+
+const StyledDiv = styled.div`
+  text-align: center;
+`;
+
+const StyledText = styled.p`
+  cursor: pointer;
+  border: solid 1px #222;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  width: 20vw;
+  height: 20vh;
+  padding: 10px;
+  margin: 10px;
+  :hover {
+    background: #22f;
+  }
+  transition: all 1s;
+`;
+
+const ButtonProps = props => {
+  return (
+    <StyledText onClick={() => props.updateClick()}>
+      Clicked {props.times}
+    </StyledText>
+  );
+};
+
+class Inicio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "rgb(255,0,255)",
+      times: 0
+    };
+  }
+
+  updateClick = () => {
+    this.setState({ times: this.state.times + 1 });
+  };
+
+  render() {
+    return (
+      <StyledDiv className="App">
+        <StyledButton color={this.state.color}>hello world</StyledButton>
+        <ButtonProps
+          updateClick={this.updateClick.bind(this)}
+          times={this.state.times}
+        />
+      </StyledDiv>
+    );
+  }
+}
+
+export default Inicio;
