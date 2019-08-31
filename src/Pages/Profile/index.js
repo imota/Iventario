461dc393296b4b67e9222d96f6daaabef0d9e93e
guibytes <guilhermenatal47@gmail.com>
2019-08-31 18:40:30 -0300

    gitflow-feature-stash: profile, image and text

--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -1,44 +1,19 @@
 import React, { Component } from "react";
-import styled from "styled-components";
+import {
+  StyledButton,
+  StyledDiv,
+  UserInformationBox,
+  ProfileImage,
+  InformationBox,
+  DefaultInformationText,
+  ProfileImageBox
+} from "./Styles";
 
-const StyledButton = styled.button`
-  border: solid 1px #222;
-  background: ${props => (props.isRed ? "#f00" : "#ff0")};
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-`;
-
-const StyledDiv = styled.div`
-  text-align: center;
-`;
-
-const StyledText = styled.p`
-  cursor: pointer;
-  border: solid 1px #222;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-  width: 20vw;
-  height: 20vh;
-  padding: 10px;
-  margin: 10px;
-  :hover {
-    background: #22f;
-  }
-  transition: all 1s;
-`;
-
-const ButtonProps = props => {
-  return (
-    <StyledText onClick={() => props.updateClick()}>
-      Clicked {props.times}
-    </StyledText>
-  );
-};
-
-class Inicio extends Component {
+class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      color: "rgb(255,0,255)",
-      times: 0
+      red: false
     };
   }
 
@@ -48,15 +23,21 @@ class Inicio extends Component {
 
   render() {
     return (
-      <StyledDiv className="App">
+      <StyledDiv>
+        <UserInformationBox>
+          <ProfileImageBox>
+            <ProfileImage src={"logo192.png"} alt="no image" />
+          </ProfileImageBox>
+          <InformationBox>
+            <DefaultInformationText>Nome</DefaultInformationText>
+            <DefaultInformationText>Email</DefaultInformationText>
+            <DefaultInformationText>Telefone</DefaultInformationText>
+          </InformationBox>
+        </UserInformationBox>
         <StyledButton isRed>hello world</StyledButton>
-        <ButtonProps
-          updateClick={this.updateClick.bind(this)}
-          times={this.state.times}
-        />
       </StyledDiv>
     );
   }
 }
 
-export default Inicio;
+export default Profile;
