a86dc8c51ade5a1b78a3d292e836906e6aec87ef
guibytes <guilhermenatal47@gmail.com>
2019-08-31 15:15:36 -0300

    adding firebase to the project

---
+++ src/firebaseConf.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC3JgkJaYyXzqIkRIDtdc-WSxgeCFmiqlo",
+  authDomain: "iventariofb.firebaseapp.com",
+  databaseURL: "https://iventariofb.firebaseio.com",
+  projectId: "iventariofb",
+  storageBucket: "iventariofb.appspot.com",
+  messagingSenderId: "525680515281",
+  appId: "1:525680515281:web:9ced600f502c29c2"
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
